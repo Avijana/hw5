b8277f384db9b26cb69f0ebd71083b2bcb4c2aa4
Avinaash Janarthanan <ajanarth@usc.edu>
2022-11-22 07:49:31 +0000

    checkpoint

--- wordle.cpp
+++ wordle.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void wHelper(std::string current, const std::set<std::string>& dict, std::set<std::string>& match, size_t pos, std::string fLetters);
+
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -19,7 +21,71 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		std::set<std::string> output;
+		size_t pos = 0;
+		wHelper(in, dict, output, pos, floating);
+		return output;
 }
 
 // Define any helper functions here
+
+void wHelper(std::string current, const std::set<std::string>& dict, std::set<std::string>& match, size_t pos, std::string fLetters)
+{
+	int numDashes = 0;
+	string fL = fLetters; //floating letters
+
+	if(pos == current.size())
+	{
+		if(fL.empty())
+		{
+			if(dict.find(current)!=dict.end())
+			{
+				match.insert(current);
+			}
+			return;
+		}
+	}
+
+	if(current[pos] != '-')
+	{
+		wHelper(current, dict,match, pos+1,fL);
+		return;
+	}
+
+	for(size_t s = 0; s < current.size(); s++)
+	{
+		if(current[s] == '-')
+		{
+			numDashes++;
+		}
+	}
+
+	if(numDashes > (int)fL.size())
+	{
+		for(char check = 'a'; check <= 'z'; check++)
+		{
+			if(fL.find(check) != string::npos)
+			{
+				string temp = fL;
+				current[pos] = check;
+				temp.erase(fL.find(check),1);
+				wHelper(current, dict, match, pos+1, temp);
+			}
+			else
+			{
+				current[pos] = check;
+				wHelper(current, dict, match, pos+1, fL);
+			}
+		}
+	}
+	else
+	{
+		for(size_t s = 0; s < fL.size(); s++)
+		{
+			string temp = fL;
+			current[pos] = fL[s];
+			temp.erase(s,1);
+			wHelper(current,dict,match,pos+1,temp);
+		}
+	}
+}
